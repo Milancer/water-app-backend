c7f8715229d88f534e275cee2e532bc345a5c4c7
Jonathan <Jonathan@innostream.co>
2025-12-03 18:18:00 +0200

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  app.enableCors();
+
   // Enable validation globally
   app.useGlobalPipes(
     new ValidationPipe({
